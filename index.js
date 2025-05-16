b017cb327ff9dd593262194532925dd529e2ccbb
bigdawgliana <liana.ortiz25@compscihigh.org>
2025-05-16 19:10:12 +0000

    ok

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const { type } = require("os");
 const app = express()
 app.use(express.static(__dirname + "/public"));
 
-app.use(express.json())
-
 app.set("view engine", "ejs")
 
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(`${req.method}: ${req.path}`);
@@ -22,12 +21,21 @@ const articleSchema = new mongoose.Schema(
         link:{type:String, required: true},
         text: {type:String, required: true},
 
-},
-
+}
 )
 
-const article = mongoose.modell("Article", articleSchema, "Articles")
+const Article = mongoose.model("Article", articleSchema, "Articles")
 
+
+app.use("/add/article", async (req, res) => {
+  const newArticle = await new Article({
+    title: req.body.title,
+    readingLevel: req.body.readingLevel,
+    link: req.body.link,
+    text: req.body.text,  
+}).save()
+   res.json(newArticle);
+ });
 
 
 app.use((req, res, next) => {
@@ -37,27 +45,45 @@ app.use((req, res, next) => {
 
 
 app.get("/", async (req, res) => {
-  res.json("home.ejs");
+  const articles = await Article.find()
+  res.render("home.ejs", {articles});
 });
 
-
-
-app.post("/")
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, () => {
-    console.log('Server running.')
-
+app.patch("/update/:_id", async (req, res) => {
+    const response = await Article.findOneAndUpdate({_id: req.params._id},   req.body, {new:true})
+  
+    res.json(response)
 })
 
+
+app.delete("/delete/:_id", async (req, res) => {
+    const resp = await Article.findOneAndDelete({_id: req.params._id})
+    res.json(resp)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function startServer() {
+   
+    await mongoose.connect("mongodb+srv://SE12:CSH2025@cluster0.8urlb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+   
+   
+
+    app.listen(3000, () => {
+        console.log(`Server running.`);
+    });
+}
+
+startServer();
